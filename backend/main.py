fe3b7ccb14f37e133e60bf4add467a41c4ccd0db
SigFaulter <107132173+SigFaulter@users.noreply.github.com.>
2025-04-16 23:57:55 +0100

    add simple auto seeding mechanism

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from db.database import engine
 from db.models import SQLModel
 from api.v1 import router
+from seed import seed_users
 
 app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
     SQLModel.metadata.create_all(engine)
+    seed_users(engine)
 
 app.include_router(router)
 
---
+++ backend/seed.py
@@ -0,0 +1,29 @@
+
+from sqlmodel import Session
+from db.models import User
+from core.utils import get_password_hash
+
+def seed_users(engine) -> None:
+    users = [
+        {
+            "username": "admin",
+            "password": get_password_hash("admin"),
+            "is_admin": True,
+            "devices": []
+        },
+        {
+            "username": "user1",
+            "password": get_password_hash("password"),
+            "is_admin": False,
+            "devices": [0]
+        }
+    ]
+
+    with Session(engine) as session:
+        for user_data in users:
+            if not session.query(User).filter(User.username == user_data["username"]).first():
+                session.add(User(**user_data))
+
+        session.commit()
+
+    print("[✓] Users seeded")
