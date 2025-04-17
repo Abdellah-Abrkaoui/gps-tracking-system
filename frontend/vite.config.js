379c992c53b61ed119e17e0b120a1d27a47b66cf
SigFaulter <107132173+SigFaulter@users.noreply.github.com.>
2025-04-17 17:06:31 +0100

    add dockerized frontend + minor tweaks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // equivalent to 0.0.0.0
+    port: 3000
+  },
+  proxy: {
+    '/api': {
+      target: 'http://backend:8000',
+      changeOrigin: true,
+      secure: false,  // TODO http set to true + configure https
+            },
+  }
 });
