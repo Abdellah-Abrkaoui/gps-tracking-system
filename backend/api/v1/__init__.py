25d91c5f3b24aa4e8abfd8166c79b2333c722af9
SigFaulter <107132173+SigFaulter@users.noreply.github.com.>
2025-04-17 23:17:10 +0100

    change prefix + add required auth dependency

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from api.v1 import user, auth
+from core.dependencies import authentication_required
 
 #
 # TODO async postgres interactions
 #
-router = APIRouter(prefix="/v1")
-router.include_router(user.router)
-router.include_router(auth.router)
+router = APIRouter(prefix="/api/v1")
+router.include_router(user.router, dependencies=[Depends(authentication_required)], tags=["User management"])
+router.include_router(auth.router, tags=["Authentication"])
