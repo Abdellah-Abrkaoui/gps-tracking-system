8908f9f311b14680dde237cffd3ffb7296e518ac
abdellah abrkaoui <abdellahabrkaoui14@gmail.com>
2025-05-04 16:19:57 +0100

    Added logic of logout automatically if oken is expired

--- frontend/src/utils/authHelper.js
+++ frontend/src/utils/authHelper.js
@@ -5,3 +5,10 @@ export const isLoggedIn = () => {
 export const getUserRole = () => {
   return localStorage.getItem("role"); // admin or user
 };
+
+export const logout = () => {
+  localStorage.removeItem("jwt");
+  localStorage.removeItem("isLoggedIn");
+  localStorage.removeItem("role");
+  window.location.href = "/login"; // force redirect to login
+};
