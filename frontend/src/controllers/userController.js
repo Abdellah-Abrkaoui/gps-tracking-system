29e2a3453e7bdd9b7d3d79b3d8200ce1b76b8729
abdellah abrkaoui <abdellahabrkaoui14@gmail.com>
2025-05-09 12:21:21 +0100

    delete unnecessary files and controllers

--- frontend/src/controllers/userController.js
+++ frontend/src/controllers/userController.js
@@ -11,19 +11,6 @@ const userController = {
     }
   },
 
-  getUserById: async (userId) => {
-    try {
-      const response = await axiosInstance.get(`/users/${userId}`);
-      return response.data;
-    } catch (error) {
-      console.error(
-        `Error fetching user with ID ${userId}:`,
-        error.response?.data?.detail
-      );
-      throw new Error(error.response?.data?.detail || "Failed to fetch user");
-    }
-  },
-
   createUser: async (userData) => {
     try {
       const response = await axiosInstance.post("/users", userData);
@@ -64,7 +51,6 @@ const userController = {
 export default userController;
 
 export const getAllUsers = userController.getAllUsers;
-export const getSingleUser = userController.getUserById;
 export const addUser = userController.createUser;
 export const editSingleUser = userController.updateUser;
 export const deleteUser = userController.deleteUser;
